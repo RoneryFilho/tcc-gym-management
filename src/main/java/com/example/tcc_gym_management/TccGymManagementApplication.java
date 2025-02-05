48be1dad5fd0165074d6acb029cb995eba2028a7
Ronery Filho <roneryrffilho@gmail.com>
2025-02-05 11:03:42 -0300

    First commit

---
+++ src/main/java/com/example/tcc_gym_management/TccGymManagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.tcc_gym_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TccGymManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TccGymManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/tcc_gym_management/TccGymManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tcc_gym_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TccGymManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
